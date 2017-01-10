d4c87bb714217e3ccfe1d83f34bc11ab89738b14
Collin Dreher <collin.dreher@gmail.com>
2017-01-10 11:05:03 -0500

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+//Collin Dreher - this is just in here so Java displays as depository language...
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println();
+		System.out.println("Hello world");
+	}
+}
